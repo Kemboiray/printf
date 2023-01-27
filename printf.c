f2f87c3520acb24fa7fc10e7db06ddf293988b31
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-01-27 19:34:58 +0300

    Revert "Ishallah. - Timestamp: 1674774177"
    This reverts commit 43dc1732a76b6b71c0d0457cde4bf14f92ff7ccc.

--- printf.c
+++ printf.c
@@ -39,6 +39,7 @@ void printcs(const char *format, int *count_ptr, va_list args)
 			format++;
 			if (*format)
 			{
+<<<<<<< HEAD
 				switch (*format)
 				{
 					case 'c':
@@ -62,6 +63,28 @@ void printcs(const char *format, int *count_ptr, va_list args)
 						*count_ptr += 2 + _putchar(*format);
 						break;
 				}
+=======
+				case 'c':
+					*count_ptr += print_char(args);
+					break;
+				case 's':
+					*count_ptr += print_str(args);
+					break;
+				case 'd':
+				case 'i':
+					*count_ptr += print_int(va_arg(args, int));
+					break;
+				case 'b':
+					*count_ptr += print_binary(va_arg(args, unsigned int));
+					break;
+				case '%':
+					*count_ptr += print_percent();
+					break;
+				default:
+					_putchar('%');
+					*count_ptr += _putchar(*format);
+					break;
+>>>>>>> parent of 43dc173... Ishallah. - Timestamp: 1674774177
 			}
 			else
 				*count_ptr = -1;
